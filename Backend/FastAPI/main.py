a334ebdc11dc90068d57a3e18824fa8122c7cca0
santiago-sz <santiago-sz@hotmail.com>
2024-07-05 20:10:39 -0300

    antes de base de datos

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import producs, users
+from routers import producs, users , jwt_auth_users , basic_auth_users
 from fastapi.staticfiles import StaticFiles
  
 app = FastAPI()
@@ -7,6 +7,11 @@ app = FastAPI()
 #routers
 app.include_router(producs.router)
 app.include_router(users.router)
+app.include_router(jwt_auth_users.router)
+app.include_router(basic_auth_users.router)
+
+
+
 app.mount ("/static", StaticFiles(directory="static"), name="statico")
 
 @app.get("/")
